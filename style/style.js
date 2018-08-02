e19b7bc1d1bec3c4fcf88801fd89d4d70660a50d
Subhi Beidas <subhi@goshippo.com>
2018-08-02 16:50:14 +0300

    removed rotation section

--- style/style.js
+++ style/style.js
@@ -1,28 +1,18 @@
 // Write all your JavaScript and JQuery code in this file! :)
 
-var x = 0; // rotation of X
-var y = 0; // rotation of Y
-var z = 0; // rotation of Z
+
 
 function updateSize(newSize) {
   // FINISH ME! Set the width of the MEET logo to be newSize in pixels.
 }
 
-function updateRotation() {
-  // FINISH ME! Set the rotation of the MEET logo based on the values of (x, y, z) (see above!)
+
+function updateLeftPosition(newX) {
+	// FINISH ME
+
 }
 
-function updateXRotation(newX) {
-  x = newX;
-  updateRotation();
+function updateTopPosition(newY) {
+	// FINISH ME
 }
 
-function updateYRotation(newY) {
-  y = newY;
-  updateRotation();
-}
-
-function updateZRotation(newZ) {
-  z = newZ;
-  updateRotation();
-}
